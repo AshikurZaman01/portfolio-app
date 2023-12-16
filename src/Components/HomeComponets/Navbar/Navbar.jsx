1da29e8d1cc1dadfd3eb0b0d40c946258eea8581
AshikurZaman01 <ashikurzaman774@gmail.com>
2023-12-16 17:09:47 +0600

    make responsive navbar

--- src/Components/HomeComponets/Navbar/Navbar.jsx
+++ src/Components/HomeComponets/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { IoHomeOutline } from "react-icons/io5";
 import { FaRegUser } from "react-icons/fa";
@@ -5,16 +6,27 @@ import { CiStickyNote } from "react-icons/ci";
 import { MdHomeRepairService } from "react-icons/md";
 import { CiPassport1 } from "react-icons/ci";
 import { MdOutlineContactMail } from "react-icons/md";
+import { RiMenu2Fill } from "react-icons/ri";
+import { IoCloseCircleOutline } from "react-icons/io5";
 
 const Navbar = () => {
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div>
-
-            <nav className="flex justify-between items-center border-b py-4">
+            <nav className={`flex flex-col md:flex-row justify-between items-center border-b py-4 ${isMenuOpen ? 'bg-gray-200' : ''}`}>
                 <div><Link className="text-5xl font-bold" to={'/'}>Ashik</Link></div>
 
-                <div>
-                    <ul className="flex items-center gap-6 text-2xl font-semibold">
+                <button onClick={toggleMenu} className="md:hidden self-start mr-4 mt-4 text-3xl z-50">
+                    {isMenuOpen ? <IoCloseCircleOutline /> : <RiMenu2Fill />}
+                </button>
+
+                <div className={`md:ml-auto ${isMenuOpen ? 'block' : 'hidden'}`}>
+                    <ul className="flex flex-col md:flex-row items-center gap-6 text-2xl font-semibold">
                         <li><Link className="flex justify-center items-center gap-2" to={'/'}><span><IoHomeOutline /></span>Home</Link></li>
 
                         <li><Link className="flex justify-center items-center gap-2" to={'/'}><span><FaRegUser /></span>About</Link></li>
@@ -29,7 +41,6 @@ const Navbar = () => {
                     </ul>
                 </div>
             </nav>
-
         </div>
     );
 };
